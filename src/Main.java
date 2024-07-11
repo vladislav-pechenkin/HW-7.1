067969a27cf4c2def7ac0b156bf0b7d4a242ba53
User <89028637729@mail.ru>
2024-07-11 11:09:28 +0500

    Домашняя работа № 7 Выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner read = new Scanner(System.in);
         System.out.println("Задача 1");
 
         int theAmount = 15000;
@@ -9,9 +11,9 @@ public class Main {
         while (total < 2_459_900) {
             month ++;
             total += theAmount;
-            System.out.println("Месяц - " + month + " сумма накоплений равна " +  total + " рублей.");
+            System.out.println("Месяц - " + month + " сумма накоплений равна " + total + " рублей.");
         }
-        System.out.println("Для накопления 2459000, потребуется "  + month + " месяцев.");
+        System.out.println("Для накопления 2459000, потребуется " + month + " месяцев.");
 
         System.out.println("Задача 2");
 
@@ -40,71 +42,74 @@ public class Main {
 
         System.out.println("Задача 4");
 
-        int month1 = 1;
-        int total1 = 0;
-        int salary = 15000;
+        int salary = theAmount;
         int maxSalary = 12_000_000;
-        while (total1 < maxSalary) {
-            total1 += total1 / 100 * 7;
-            total1 += salary;
-            System.out.println("За месяц № " + month1 + " сумма накопления = " + total1 + " рублей.");
+        double total1 = 1.07;
+        int month1 = 0;
+        while (salary < maxSalary) {
+            salary *= total1;
             month1++;
+            System.out.println("За месяц № " + month1 + " сумма накопления = " + salary + " рублей.");
         }
-        System.out.println("Василий за " + month1 + " месяц, накопит  сумму в " + total1 + " рублей.");
+        System.out.println("Василий за " + month1 + " месяцев, накопит сумму в " + salary + " рублей.");
 
         System.out.println("Задача 5");
 
-        int month2 = 1;
-        int total2 = 0;
-        int salary1 = 15000;
-        for (; total2 <= 12_000_000; month2++) {
-            total2 += total2 / 100 * 7;
-            total2 += salary1;
+        int salary1 = theAmount;
+        int maxSalary1 = 12_000_000;
+        int month2 = 0;
+        while (salary1 < maxSalary1){
+            salary1 *= total1;
+            month2++;
             if (month2 % 6 == 0) {
-                System.out.println("За месяц № " + month2 + " сумма накопления = " + total2 + " рублей.");
+                System.out.println("За месяц № " + month2 + " сумма накопления = " + salary1 + " рублей.");
             }
         }
-        System.out.println("Василий за " + month2 + " месяц, накопит  сумму в " + total2 + " рублей.");
+        System.out.println("Василий за " + month2 + " месяц, накопит сумму в " + salary1 + " рублей.");
 
         System.out.println("Задача 6");
 
-        int forTheYear1 = 9;
-        int total3 = 0;
-        int salary2 = 15000;
-        int monthsInYear = 12;
-        for (int month3 = 1; month3 <= forTheYear1 * monthsInYear; month3++) {
-            total3 += total3 / 100 * 7;
-            total3 += salary2;
+        int salary2 = theAmount;
+        for (int month3 = 1; month3 <= (9 * 12); month3++) {
+            salary2 *= total1;
             if (month3 % 6 == 0) {
-                System.out.println("За каждые полгода сумма накопления = " + total3 + " рублей.");
+                System.out.println("За каждые полгода сумма накопления = " + salary2 + " рублей.");
             }
         }
-        System.out.println("Василий за 9 лет накопит сумму в " + total3 + " рублей.");
+        System.out.println("Василий за 9 лет накопит сумму в " + salary2 + " рублей.");
 
         System.out.println("Задача 7");
 
-        int firstFriday = 5;
-        int daysMouth = 31;
-        int friday = firstFriday;
-        int week = 7;
-        while (friday <= daysMouth) {
-            System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет.");
-            friday += week;
+        System.out.println("Введите число первой пятницы: ");
+        int firstFriday = read.nextInt();
+        int day = 0;
+
+        if (firstFriday > 7) {
+            return;
+        }
+        System.out.println("Введите колличество дней в месяце: ");
+        int daysMonth = read.nextInt();
+
+        if (daysMonth <= 27 || daysMonth > 31) {
+            return;
+        }
+
+        for (day = firstFriday; day <= daysMonth; day+= 7){
+            System.out.println("Сегодня пятница, " + day + "-е число. Необходимо подготовить отчет.");
         }
 
         System.out.println("Задача 8");
 
-        int kamets = 79;
-        int date1 = 200;
-        int date2 = 100;
-        int currentYear = 2023;
-        int pastBecentenary = currentYear - date1;
-        for (int year = 0; year <= currentYear + date2; year = year + kamets) {
-            if (year >= pastBecentenary) {
-                System.out.println(year);
-                if (year > currentYear) {
-                    break;}
+        System.out.println("Введите год");
+        int year = read.nextInt();
+        int lastYear = year - 200;
+        int nextYear = year + 100;
+        int year2 = 0;
+        for (year2 = lastYear; year2 <= nextYear; year2++) {
+            if (year2 % 79 == 0) {
+                System.out.println(year2);
             }
         }
+        read.close();
     }
 }
